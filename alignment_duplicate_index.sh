9559fce23c3eced5df44981cd286a3a397153c39
schosio <94955130+schosio@users.noreply.github.com>
2021-12-09 15:22:20 +0530

    Update alignment_duplicate_index.sh

--- alignment_duplicate_index.sh
+++ alignment_duplicate_index.sh
@@ -12,7 +12,7 @@ splicefile=/home/chaos/draic_data_analysis/annotation_file/gencode.v38.splice.tx
 for i in *.fastq
 do
   	date
-	name=$(echo $i | awk -F"_" '{print $i}')
+	name=$(echo $i | awk -F"_" '{print $1}')
         echo $name
         #display the command used
         echo  "hisat2 -p $threads -x $idx --known-splicesite-infile $splicefile $i | samtools view -bS - | samtools sort -n - -o $name.sorted.bam"
@@ -25,7 +25,7 @@ done
 for j in *.sorted.bam
 do
   	    date
-	      name1=$(echo $j | awk -F"_" '{print $i}')
+	      name1=$(echo $j | awk -F"_" '{print $1}')
         echo $name1
         echo "samtools fixmate -m $j - | samtools sort - | samtools markdup -rs - $name1.rmPCRdup.bam"
         #options for samtools: fixmate is for fix mate information, markdup is for marking duplicates       
