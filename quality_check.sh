91d5bc3a2f7e3e20e8786999b0c4403f1e606ef3
schosio <94955130+schosio@users.noreply.github.com>
2021-12-13 12:44:55 +0530

    Update quality_check.sh

--- quality_check.sh
+++ quality_check.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 #[find] will find any file with extension .fastq
 #the output of [find] will be piped to parallel
 #parallel will use multi cores to run fastqc
 #options for find : -name is to search based on name with name as an argument in "" 
 #options for parallel : the -v option is for verbose, -I% option is for , --max-args is for maximum arguments parallel can take input simultaniously
 #options for fastqc: --extract is for unziping the output file
-find . -name "*.fastq" | parallel -v -I% --max-args 1 fastqc --extract
-
+find . -name "*.fastq" | parallel -j 5 -v -I% --max-args 1 fastqc 
+find . -name "*.fastq.gz" | parallel -j 5 -v -I% --max-args 1 fastqc 
